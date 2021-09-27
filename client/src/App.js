4807c7c18843e1da741ffa9fa5af975ab055df62
GrimonprezAlexis <70689985+alexgrz.dev@users.noreply.github.com>
2021-09-27 21:37:49 +0200

    Show user when login

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import './App.scss'
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import store from './store'
 import Home from './components/Home/home'
 import SignIn from './components/SignIn/signin'
 import User from './components/User/user'
-
 
 class App extends Component {
 
--- client/src/components/Navigation/navigation.js
+++ client/src/components/Navigation/navigation.js
@@ -5,18 +5,18 @@ const Navigation = ({ profile }) => {
 
     return (
         <>
-        <nav class="main-nav">
-            <a class="main-nav-logo" href="./index.html">
+        <nav className="main-nav">
+            <a className="main-nav-logo" href="./index.html">
             <img
-                class="main-nav-logo-image"
+                className="main-nav-logo-image"
                 src={`${window.location.origin}/img/argentBankLogo.png`}
                 alt="Argent Bank Logo"
             />
-            <h1 class="sr-only">Argent Bank</h1>
+            <h1 className="sr-only">Argent Bank</h1>
             </a>
             <div>
-                { profile && <Link to='/user' className="main-nav-item"><i class="fa fa-user-circle"></i> {profile.firstName} {profile.lastName}</Link> }
-                { !profile && <Link to='/signin' className="main-nav-item"><i class="fa fa-user-circle"></i> Sign In</Link> }
+                { profile && <Link to='/user' className="main-nav-item"><i className="fa fa-user-circle"></i> {profile.firstName} {profile.lastName}</Link> }
+                { !profile && <Link to='/signin' className="main-nav-item"><i className="fa fa-user-circle"></i> Sign In</Link> }
             </div>
         </nav>
         </>
